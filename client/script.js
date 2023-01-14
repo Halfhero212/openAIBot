c4e8b3d8ef21781005bf3ef08027892bf04be0c5
Noor Alhussain <84689430+Halfhero212@users.noreply.github.com>
2023-01-14 12:18:38 +0300

    update the server url again

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://breadbot-usum.onrender.com', {
+    const response = await fetch('https://breadbot-5fji.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
